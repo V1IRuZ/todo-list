62d9eda26bc54c0c8b850fff508c3d27919fb295
Esa <esa-pitkanen@hotmail.com>
2025-04-11 19:07:40 +0300

    Add project class
    A project class was created that contains the project's "to do" tasks in an array. It has a method that can add a new "to do" to the array.

--- src/new-todo.js
+++ src/new-todo.js
@@ -1,3 +1,15 @@
+class Project {
+    toDos = []
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    addTodo(todo) {
+        this.toDos.push(todo)
+    }
+}
+
 class Todo {
     constructor(title, description, dueDate, priority) {
         this.title = title;
@@ -7,8 +19,14 @@ class Todo {
     }
 }
 
+const project = new Project("Everyday tasks");
+
 const todo = new Todo("Workout", "Leg day", "12.4.2025", "high");
+const medicine = new Todo("Allergic Medicine", "1 tablet of certizin", "12.4.2025", "Critical")
+
+project.addTodo(todo);
+project.addTodo(medicine);
 
 export function printTodo () {
-    console.log(todo);
+    console.log(project);
 };
